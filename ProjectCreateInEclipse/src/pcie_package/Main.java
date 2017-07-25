74181d3b86c178427cd62648df1372f041a2bf2c
AndriyCO <Aolijnyk73@gmail.com>
2017-07-25 17:14:34 +0300

    last variant

--- ProjectCreateInEclipse/src/pcie_package/Main.java
+++ ProjectCreateInEclipse/src/pcie_package/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 
 		System.out.println("We staed at some fancy hotel in center London");
-
+		System.out.println("and this");
 		System.out.println("++++++++++++++++++++++++++++");
 		System.out.println("collateral - nit means this branch is paralel to master.");
 		System.out.println("I have an apointment with my lawyer next saturday");
